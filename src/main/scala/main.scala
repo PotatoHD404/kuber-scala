337f12ed1187f17b0511ebb6edaf4178e0147396
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2023-04-28 12:52:08 +0300

    intermediate changes

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -22,6 +22,19 @@ def formatDuration(duration: Duration): String = {
 
   s"${if (days > 0) days + "d" else ""}${if (hours > 0) hours + "h" else ""}${if (minutes > 0) minutes + "m" else ""}${if (seconds > 0) seconds + "s" else ""}".trim
 }
+// from format to duration
+def toDuration(value: String): Option[Duration] = {
+  if (value.isEmpty) None
+  else {
+    val days = value.split("d").headOption.map(_.toInt)
+    val hours = value.split("h").headOption.map(_.toInt)
+    val minutes = value.split("m").headOption.map(_.toInt)
+    val seconds = value.split("s").headOption.map(_.toInt)
+
+    val duration = Duration.ofDays(days.getOrElse(0)).plusHours(hours.getOrElse(0)).plusMinutes(minutes.getOrElse(0)).plusSeconds(seconds.getOrElse(0))
+    Some(duration)
+  }
+}
 
 def toOption(value: String): Option[String] = {
   if (value.isEmpty) None
@@ -36,7 +49,41 @@ def stringToFloat(value: String): Option[Float] = {
   }
 }
 
-case class MyPod(ip: Option[String], name: String, status: Option[String], startedAt: Option[String], age: Option[String], restarts: Int, states: List[String], allocatedResources: Map[String, List[BigDecimal]], namespace: String)
+case class MyEvent( reason: Option[String],
+                    message: Option[String],
+                    source: Option[String],
+                    firstSeen: String,
+                    lastSeen: String,
+                    count: Option[Int],
+                    `type`: Option[String])
+
+object MyEvent {
+  def fromEvent(event: Event): MyEvent = {
+    MyEvent(
+      reason = event.reason,
+      message = event.message,
+      source = event.source.get.component.map(identity),
+      firstSeen = event.firstTimestamp.get.toString,
+      lastSeen = event.lastTimestamp.get.toString,
+      count = event.count,
+      `type` = event.`type`
+    )
+  }
+}
+
+case class MyPod(ip: Option[String],
+                 name: String, status: Option[String],
+                 startedAt: Option[String],
+                 createdAt: Option[String],
+                 age: Option[String],
+                 ageInSec: Long,
+                 restarts: Int,
+                 states: List[String],
+                 allocatedResources: Map[String, BigDecimal],
+                 namespace: String,
+                 isSystem: Boolean,
+                 failedScheduling: Boolean,
+                 eventList: List[MyEvent])
 
 object MyPod {
   def fromPod(pod: Pod): MyPod = {
@@ -46,38 +93,57 @@ object MyPod {
     val Memory = "memory"
     val EphemeralStorage = "ephemeral-storage"
     val HugePagesPrefix = "hugepages-" // hugepages-2Mi, hugepages-1Gi
+    var duration: Duration = Duration.ZERO
+    var events: List[MyEvent] = List.empty
+    println(pod.status.get.message)
+    var _ = pod.status.get.message
     MyPod(
       ip = pod.status.get.podIP,
       name = pod.name,
       status = pod.status.get.phase.map(_.toString),
+      createdAt = pod.metadata.creationTimestamp.map(_.toString),
       startedAt = pod.status.get.startTime.map(_.toString),
-      age = pod.status.get.startTime.map { startTime =>
+      age = pod.metadata.creationTimestamp.map { createdAt =>
         val now = ZonedDateTime.now()
-        val age = Duration.between(startTime, now)
-        formatDuration(age)
+        duration = Duration.between(createdAt, now)
+        formatDuration(duration)
       },
+      ageInSec = duration.toSeconds,
       restarts = pod.status.get.containerStatuses.map(_.restartCount).sum,
       states = pod.status.get.containerStatuses.map { containerStatus =>
         containerStatus.state.get.id
       },
-      allocatedResources  = Map(
-        "cpu-requests" -> resources.flatMap(_.requests.get(CPU)).map(_.amount),
-        "cpu-limits" -> resources.flatMap(_.limits.get(CPU)).map(_.amount),
-        "memory-requests" -> resources.flatMap(_.requests.get(Memory)).map(_.amount),
-        "memory-limits" -> resources.flatMap(_.limits.get(Memory)).map(_.amount),
-        "ephemeral-storage-requests" -> resources.flatMap(_.requests.get(EphemeralStorage)).map(_.amount),
-        "ephemeral-storage-limits" -> resources.flatMap(_.limits.get(EphemeralStorage)).map(_.amount),
-        "hugepages-2Mi-requests" -> resources.flatMap(_.requests.get(HugePagesPrefix + "2Mi")).map(_.amount),
-        "hugepages-2Mi-limits" -> resources.flatMap(_.limits.get(HugePagesPrefix + "2Mi")).map(_.amount),
-        "hugepages-1Gi-requests" -> resources.flatMap(_.requests.get(HugePagesPrefix + "1Gi")).map(_.amount),
-        "hugepages-1Gi-limits" -> resources.flatMap(_.limits.get(HugePagesPrefix + "1Gi")).map(_.amount),
+      allocatedResources = Map(
+        "cpu-requests" -> resources.flatMap(_.requests.get(CPU)).map(_.amount).sum,
+        "cpu-limits" -> resources.flatMap(_.limits.get(CPU)).map(_.amount).sum,
+        "memory-requests" -> resources.flatMap(_.requests.get(Memory)).map(_.amount).sum,
+        "memory-limits" -> resources.flatMap(_.limits.get(Memory)).map(_.amount).sum,
+        "ephemeral-storage-requests" -> resources.flatMap(_.requests.get(EphemeralStorage)).map(_.amount).sum,
+        "ephemeral-storage-limits" -> resources.flatMap(_.limits.get(EphemeralStorage)).map(_.amount).sum,
+        "hugepages-2Mi-requests" -> resources.flatMap(_.requests.get(HugePagesPrefix + "2Mi")).map(_.amount).sum,
+        "hugepages-2Mi-limits" -> resources.flatMap(_.limits.get(HugePagesPrefix + "2Mi")).map(_.amount).sum,
+        "hugepages-1Gi-requests" -> resources.flatMap(_.requests.get(HugePagesPrefix + "1Gi")).map(_.amount).sum,
+        "hugepages-1Gi-limits" -> resources.flatMap(_.limits.get(HugePagesPrefix + "1Gi")).map(_.amount).sum,
       ),
-      namespace = pod.namespace
+      namespace = pod.namespace,
+      isSystem = pod.namespace.equals("kube-system"),
+      // get events list and check if there is a failed scheduling event
+      failedScheduling = false
+//      failedScheduling = pod.metadata.uid.map { uid =>
+//        val events = k8s.events.listInNamespace(pod.namespace).filter(_.involvedObject.uid.contains(uid))
+//        events.exists(_.reason.contains("FailedScheduling"))
+//      }.getOrElse(false)
     )
   }
 }
 
-case class MyNode(name: String, status: Map[String, Boolean], pods: List[MyPod], ip: Map[String, String], allocatedResources: Map[String, BigDecimal], capacity: Map[String, BigDecimal], allocatable: Map[String, BigDecimal])
+case class MyNode(name: String,
+                  status: Map[String, Boolean],
+                  pods: List[MyPod],
+                  ip: Map[String, String],
+                  allocatedResources: Map[String, BigDecimal],
+                  capacity: Map[String, BigDecimal],
+                  allocatable: Map[String, BigDecimal])
 
 object MyNode {
   def fromNode(node: Node, pods: List[Pod]): MyNode = {
@@ -99,21 +165,17 @@ object MyNode {
       }
 
       }.toMap,
-      pods = newPods.filterNot(_.namespace.equals("kube-system")),
+      pods = newPods.filterNot(_.isSystem),
       ip = node.status.get.addresses.map(a => a.`_type` -> a.address).toMap,
-      allocatedResources = newPods.flatMap(_.allocatedResources).groupBy(_._1).map { case (k, v) => k -> v.flatMap(_._2).sum },
-//      totalCpuRequests = newPods.flatMap(_.allocatedResources.get("cpu-requests")).sum,
-//      totalCpuLimits = newPods.flatMap(_.cpuLimits).sum,
-//      totalMemoryRequests = newPods.flatMap(_.memoryRequests).sum,
-//      totalMemoryLimits = newPods.flatMap(_.memoryLimits).sum,
+      allocatedResources = newPods.flatMap(_.allocatedResources).groupBy(_._1).map { case (k, v) => k -> v.map(_._2).sum },
+
       capacity = node.status.get.capacity.map((k, v) => k -> v.amount),
       allocatable = node.status.get.allocatable.map((k, v) => k -> v.amount),
     )
-    //      ip = node.status.get.capacity,
   }
 }
 
-case class KuberInfo (nodes: Map[String, MyNode], podsWithoutNode: List[MyPod])
+case class KuberInfo(nodes: Map[String, MyNode], podsWithoutNode: List[MyPod])
 
 object KuberInfo {
   def fromNodesAndPods(nodes: List[Node], pods: List[Pod]): KuberInfo = {
@@ -127,7 +189,7 @@ object KuberInfo {
 }
 
 object MyJsonProtocol extends DefaultJsonProtocol {
-  implicit val myPodFormat: RootJsonFormat[MyPod] = jsonFormat9(MyPod.apply)
+  implicit val myPodFormat: RootJsonFormat[MyPod] = jsonFormat14(MyPod.apply)
   implicit val myNodeFormat: RootJsonFormat[MyNode] = jsonFormat7(MyNode.apply)
   implicit val kuberInfoFormat: RootJsonFormat[KuberInfo] = jsonFormat2(KuberInfo.apply)
 }
@@ -154,7 +216,7 @@ def main(): Unit = {
     val namespaces = Await.result(k8s.list[NamespaceList](), 10.seconds)
     val pods = Await.result(Future.sequence(namespaces.items.map(el => k8s.list[PodList](Some(el.name)))), 10.seconds).flatten
     val info = KuberInfo.fromNodesAndPods(nodes, pods)
-//    pods.filter(_.spec.get.nodeName.isEmpty).filterNot(_.namespace.equals("kube-system")).map(_.spec.get.containers.flatMap(_.resources)).foreach(println)
+    //    pods.filter(_.spec.get.nodeName.isEmpty).filterNot(_.namespace.equals("kube-system")).map(_.spec.get.containers.flatMap(_.resources)).foreach(println)
 
     info.toJson.prettyPrint.foreach(println)
 
