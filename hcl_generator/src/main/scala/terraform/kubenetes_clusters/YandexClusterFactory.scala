e8755b389675bbe9bda921a7591017bec6751fd4
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2024-04-24 21:34:09 +0300

    tmp

--- hcl_generator/src/main/scala/terraform/kubenetes_clusters/YandexClusterFactory.scala
+++ hcl_generator/src/main/scala/terraform/kubenetes_clusters/YandexClusterFactory.scala
@@ -1,16 +1,27 @@
 package terraform.kubenetes_clusters
 
+import terraform.providers.yandex.datasources.yandex_compute_image.YandexComputeImage
+import terraform.providers.yandex.resources.yandex_vpc_network.YandexVpcNetwork
+import terraform.providers.yandex.resources.yandex_vpc_subnet.YandexVpcSubnet
 import terraform.{BackendResource, InfrastructureResource, ProviderSettings}
 import terraform.providers.yandex.{Yandex, YandexProviderConfig}
 import terraform.providers.yandex.yandexprovidersettings.YandexProviderSettings
 
+case class YandexVMFactory(image: YandexComputeImage, subnet: YandexVpcSubnet) {
+  def create: List[InfrastructureResource[Yandex]] = {
+    List[InfrastructureResource[Yandex]]()
+  }
+}
+
 case class YandexClusterFactory[
   T1 <: ProviderSettings[Yandex],
   T2 <: BackendResource,
-  T3 <: InfrastructureResource[Yandex]
-] (provider: T1, backend: Option[T2] = None){
-  def get: YandexProviderConfig[T1, T2, T3] = {
-    val resources = List[T3]()
+](provider: T1, backend: Option[T2] = None) {
+  def create: YandexProviderConfig[T1, T2, InfrastructureResource[Yandex]] = {
+    val image = YandexComputeImage("family_images_linux", family = Some("ubuntu-2004-lts"))
+    val network = YandexVpcNetwork("foo")
+    val subnet = YandexVpcSubnet("foo", networkId = network.id, v4CidrBlocks = "10.5.0.0/24" :: Nil)
+    val resources: List[InfrastructureResource[Yandex]] = image :: Nil
     YandexProviderConfig(provider, backend, resources)
   }
 }
